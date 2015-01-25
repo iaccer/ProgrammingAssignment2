0e5b46d8b3786db605478945a3c111bab157675d
iaccer <iaccer@users.noreply.github.com>
2015-01-25 20:43:11 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function (x = matrix(nrow =0, ncol =0)) {
+        minv <- matrix(nrow =0, ncol =0)
+        set <- function(y) {
+			if (!is.matrix(y))
+			{
+				end("argument not a matrix, assignment rejected")
+			}
+			x <<- y 
+			minv <<- matrix(nrow =0, ncol =0)		    		
+        }
+        get <- function() x
+        setinverse <- function(minverse) minv  <<- minverse
+        getinverse <- function() minv 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
-}
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        minv <- x$getinverse()
+        if(length(minv) > 0) {
+                message("getting cached data")
+                return(minv)
+        }
+        data <- x$get()
+        minv <- solve(data)
+        x$setinverse(minv)
+        minv
+
 }
